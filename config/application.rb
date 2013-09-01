7aa065fd5b40cfdda0f11d3774e41dca29a1fc85
Stevo <b.kosmowski@selleo.com>
2013-09-01 14:51:50 +0200

    Setup sendgrid

--- config/application.rb
+++ config/application.rb
@@ -37,13 +37,14 @@ module Bsa
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
 
-    config.action_mailer.default_url_options = { host: 'localhost:3000' }
-    config.action_mailer.delivery_method = :smtp
-    config.action_mailer.smtp_settings   = {address:              "smtp.gmail.com",
-                                            port:                 587,
-                                            user_name:            ENV["GMAIL_USERNAME"],
-                                            password:             ENV["GMAIL_PASSWORD"],
-                                            authentication:       'plain',
-                                            enable_starttls_auto: true}
+    ActionMailer::Base.smtp_settings         = {
+      address:              'smtp.sendgrid.net',
+      port:                 '587',
+      authentication:       :plain,
+      user_name:            ENV['SENDGRID_USERNAME'],
+      password:             ENV['SENDGRID_PASSWORD'],
+      domain:               'heroku.com',
+      enable_starttls_auto: true
+    }
   end
 end
