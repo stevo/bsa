833d5984eb59174b89688b1eee9f257a70518934
Stevo <b.kosmowski@selleo.com>
2013-09-01 15:17:03 +0200

    Configure i18n (PL)

--- config/application.rb
+++ config/application.rb
@@ -35,7 +35,7 @@ module Bsa
 
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
-    # config.i18n.default_locale = :de
+    config.i18n.default_locale = :pl
 
     ActionMailer::Base.smtp_settings         = {
       address:              'smtp.sendgrid.net',
