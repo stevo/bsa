d952f5cc935839efed367db9da859196cab8ebed
Stevo <b.kosmowski@selleo.com>
2013-09-03 00:05:52 +0200

    Allow creating contribution

--- app/controllers/admin/user/contributions_controller.rb
+++ app/controllers/admin/user/contributions_controller.rb
@@ -2,4 +2,10 @@ class Admin::User::ContributionsController < Admin::AdminController
   inherit_resources
   belongs_to :user
   actions :index, :new, :create, :destroy
+
+  private
+
+  def permitted_params
+    params.permit(contribution: [:amount])
+  end
 end
--- app/models/contribution.rb
+++ app/models/contribution.rb
@@ -1,2 +1,3 @@
 class Contribution < ActiveRecord::Base
+  validates :amount, numericality: {greater_than: 0}, presence: true
 end
