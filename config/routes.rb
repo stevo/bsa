e306f846566c75ca3441cf3228d555d3f072f8e6
Stevo <b.kosmowski@selleo.com>
2013-09-02 23:25:25 +0200

    add contributions to routing

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Bsa::Application.routes.draw do
   root :to => "home#index"
-  devise_for :users, :controllers => {:registrations => "registrations"}
-  resources :users
+  devise_for :users, controllers: {:registrations => "registrations"}
+
+  resources :users do
+    resources :contributions
+  end
 end
