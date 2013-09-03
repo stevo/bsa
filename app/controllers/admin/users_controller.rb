b3fa45d473ae8683cbc6c95c230be6fa2f8cfe6c
Stevo <b.kosmowski@selleo.com>
2013-09-03 22:58:13 +0200

    Re-organize admin users listing

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,13 +1,10 @@
 class Admin::UsersController < ApplicationController
   expose(:decorated_collection) { User.all.decorate }
-
-  def show
-    @user = User.find(params[:id])
-  end
+  expose(:user, model: User)
+  expose(:decorated_user){ user.decorate }
 
   def update
-    @user = User.find(params[:id])
-    if @user.update_attributes(params[:user], :as => :admin)
+    if user.save
       redirect_to users_path, :notice => "User updated."
     else
       redirect_to users_path, :alert => "Unable to update user."
@@ -15,12 +12,17 @@ class Admin::UsersController < ApplicationController
   end
 
   def destroy
-    user = User.find(params[:id])
     unless user == current_user
       user.destroy
       redirect_to users_path, :notice => "User deleted."
     else
       redirect_to users_path, :notice => "Can't delete yourself."
     end
+  end
+
+  private
+
+  def permitted_params
+    params.require(:user).permit(:email, :name)
   end
 end
