1baaf07425b172ebd61af82360b5832979d5a087
Stevo <b.kosmowski@selleo.com>
2013-09-03 15:59:35 +0200

    Configure state machine

--- app/models/membership.rb
+++ app/models/membership.rb
@@ -1,3 +1,9 @@
 class Membership < ActiveRecord::Base
   belongs_to :user
+
+  state_machine :state, initial: :new do
+    event :activate do
+      transition :new => :active
+    end
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130902212048) do
+ActiveRecord::Schema.define(version: 20130903134713) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,6 +19,15 @@ ActiveRecord::Schema.define(version: 20130902212048) do
   create_table "contributions", force: true do |t|
     t.decimal  "amount"
     t.integer  "user_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "memberships", force: true do |t|
+    t.integer  "user_id"
+    t.date     "approved_at"
+    t.decimal  "monthly_contribution"
+    t.string   "state"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
