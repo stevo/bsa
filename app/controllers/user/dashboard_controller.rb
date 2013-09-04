3f922ba2c994a81522d1603b8f47d72250f2740f
Stevo <b.kosmowski@selleo.com>
2013-09-04 13:01:45 +0200

    Display person subject to voting for membership

--- app/controllers/user/dashboard_controller.rb
+++ app/controllers/user/dashboard_controller.rb
@@ -1,3 +1,4 @@
 class User::DashboardController < AuthenticatedController
   expose(:current_user__){ current_user.decorate }
+  expose(:voting){ Voting.available_to_answer_for(current_user).order("RANDOM()").first }
 end
--- app/models/voting.rb
+++ app/models/voting.rb
@@ -1,3 +1,14 @@
 class Voting < ActiveRecord::Base
   belongs_to :membership
+  has_one :candidate, class_name: User, through: :membership, source: :user
+
+  delegate :name, to: :candidate, prefix: true
+
+  scope :available_to_answer_for, ->(voter) do
+    _scope = active.not_for(voter)
+    voter.admin? ? _scope : _scope.where(closed: true)
+  end
+
+  scope :active, -> { joins(:membership).where('memberships.state' => 'being_polled') }
+  scope :not_for, ->(candidate) { joins(:membership).where("memberships.user_id <> ?", candidate.id) }
 end
