24e3e0afe0cc0ee4845c6d98e1f8f51dfa094954
Stevo <b.kosmowski@selleo.com>
2013-09-04 16:03:26 +0200

    Refactor user specs

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,103 +1,97 @@
 require 'spec_helper'
 
 describe User do
-
-  before do
-    @attr = {
-      :name                  => "Example User",
-      :email                 => "user@example.com",
-      :password              => "changeme",
-      :password_confirmation => "changeme"
+  let(:attrs) do
+    {
+      name:                  "Example User",
+      email:                 "user@example.com",
+      password:              "changeme",
+      password_confirmation: "changeme"
     }
   end
 
-  it "should create a new instance given a valid attribute" do
-    User.create!(@attr)
+  it "creates a new instance given a valid attribute" do
+    User.create!(attributes_for(:user))
   end
 
-  it "should require an email address" do
-    no_email_user = User.new(@attr.merge(:email => ""))
-    no_email_user.should_not be_valid
-  end
+  context "validations" do
+    context "user has no email address" do
+      let(:user) { build(:user, email: '') }
+      it { expect(user).to_not be_valid }
+    end
 
-  it "should accept valid email addresses" do
-    addresses = %w[user@foo.com THE_USER@foo.bar.org first.last@foo.jp]
-    addresses.each do |address|
-      valid_email_user = User.new(@attr.merge(:email => address))
-      valid_email_user.should be_valid
+    context "user has valid email address" do
+      %w[user@foo.com THE_USER@foo.bar.org first.last@foo.jp].each do |valid_address|
+        let(:user) { build(:user, email: valid_address) }
+        it { expect(user).to be_valid }
+      end
+    end
+
+    context "user has invalid email address" do
+      %w[user@foo,com user_at_foo.org example.user@foo.].each do |valid_address|
+        let(:user) { build(:user, email: valid_address) }
+        it { expect(user).to_not be_valid }
+      end
+    end
+
+    context "user already exists with given email address" do
+      let(:duplicate_address) { 'duplicate@address.com' }
+      before { create(:user, email: duplicate_address) }
+      let(:user){ build(:user, email: duplicate_address) }
+      it { expect(user).to_not be_valid }
     end
   end
 
-  it "should reject invalid email addresses" do
-    addresses = %w[user@foo,com user_at_foo.org example.user@foo.]
-    addresses.each do |address|
-      invalid_email_user = User.new(@attr.merge(:email => address))
-      invalid_email_user.should_not be_valid
-    end
-  end
-
-  it "should reject duplicate email addresses" do
-    User.create!(@attr)
-    user_with_duplicate_email = User.new(@attr)
-    user_with_duplicate_email.should_not be_valid
-  end
-
-  it "should reject email addresses identical up to case" do
-    upcased_email = @attr[:email].upcase
-    User.create!(@attr.merge(:email => upcased_email))
-    user_with_duplicate_email = User.new(@attr)
+  it "rejects email addresses identical up to case" do
+    upcased_email = attrs[:email].upcase
+    User.create!(attrs.merge(:email => upcased_email))
+    user_with_duplicate_email = User.new(attrs)
     user_with_duplicate_email.should_not be_valid
   end
 
   describe "passwords" do
+    let(:user) { User.new(attrs) }
 
-    before do
-      @user = User.new(@attr)
-    end
 
     it "should have a password attribute" do
-      @user.should respond_to(:password)
+      user.should respond_to(:password)
     end
 
     it "should have a password confirmation attribute" do
-      @user.should respond_to(:password_confirmation)
+      user.should respond_to(:password_confirmation)
     end
   end
 
   describe "password validations" do
 
-    it "should require a password" do
-      User.new(@attr.merge(:password => "", :password_confirmation => "")).
+    it "requires a password" do
+      User.new(attrs.merge(:password => "", :password_confirmation => "")).
         should_not be_valid
     end
 
-    it "should require a matching password confirmation" do
-      User.new(@attr.merge(:password_confirmation => "invalid")).
+    it "requires a matching password confirmation" do
+      User.new(attrs.merge(:password_confirmation => "invalid")).
         should_not be_valid
     end
 
-    it "should reject short passwords" do
+    it "rejects short passwords" do
       short = "a" * 5
-      hash  = @attr.merge(:password => short, :password_confirmation => short)
+      hash  = attrs.merge(:password => short, :password_confirmation => short)
       User.new(hash).should_not be_valid
     end
 
   end
 
   describe "password encryption" do
-
-    before do
-      @user = User.create!(@attr)
-    end
+    let!(:user) { User.create!(attrs) }
 
     it "should have an encrypted password attribute" do
-      @user.should respond_to(:encrypted_password)
+      user.should respond_to(:encrypted_password)
     end
 
     it "should set the encrypted password attribute" do
-      @user.encrypted_password.should_not be_blank
+      user.encrypted_password.should_not be_blank
     end
-
   end
 
 
@@ -109,7 +103,7 @@ describe User do
     end
 
     context "user has membership" do
-      before { create(:membership, user_id: subject.id)}
+      before { create(:membership, user_id: subject.id) }
 
       it { expect(subject.guest?).to be_false }
     end
