e3bce546e59faf1876e35a47f9a383b7785844ce
Stevo <b.kosmowski@selleo.com>
2013-09-04 15:51:07 +0200

    optimize naming

--- spec/models/contribution_spec.rb
+++ spec/models/contribution_spec.rb
@@ -1,14 +1,14 @@
 require 'spec_helper'
 
 describe Contribution do
-  describe "validations" do
+  context "validations" do
     context "with valid attributes" do
-      subject { build(:contribution)}
+      subject { build(:contribution) }
       it { expect(subject).to be_valid }
     end
 
     context "with no amount" do
-      subject { build(:contribution, amount: nil)}
+      subject { build(:contribution, amount: nil) }
       it { expect(subject).to_not be_valid }
     end
   end
