7b841f3952320422b0729714592cfb26456f9999
Stevo <b.kosmowski@selleo.com>
2013-09-04 13:00:50 +0200

    Fix formatting

--- app/decorators/user_decorator.rb
+++ app/decorators/user_decorator.rb
@@ -21,11 +21,11 @@ class UserDecorator < Draper::Decorator
     if object.guest?
       h.content_tag(:div, I18n.t('activemodel.user.guest_alert'), class: 'alert alert-warning') <<
         h.content_tag(:p, class: 'text-center') do
-        h.link_to I18n.t('activemodel.user.join_association_link'), h.user_membership_path, method: :post, class: 'btn btn-success btn-lg'
-          end
-        elsif !object.membership_approved?
-        h.content_tag(:div,I18n.t("activemodel.membership.statuses.#{object.membership_state}.description"), class: 'alert alert-info')
+          h.link_to I18n.t('activemodel.user.join_association_link'), h.user_membership_path, method: :post, class: 'btn btn-success btn-lg'
         end
+    elsif !object.membership_approved?
+      h.content_tag(:div, I18n.t("activemodel.membership.statuses.#{object.membership_state}.description"), class: 'alert alert-info')
+    end
   end
 
 end
