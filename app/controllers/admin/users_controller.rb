e1111530733247ae408b84b51448a083ac38b9c0
Stevo <b.kosmowski@selleo.com>
2013-09-05 20:50:24 +0200

    admin/users controller specs

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,28 +1,28 @@
 class Admin::UsersController < ApplicationController
   expose(:decorated_collection) { User.all.decorate }
-  expose(:user, model: User)
+  expose(:user, model: User, attributes: :permitted_params)
   expose(:decorated_user){ user.decorate }
 
   def update
     if user.save
-      redirect_to users_path, :notice => "User updated."
+      redirect_to admin_users_path, :notice => "User updated."
     else
-      redirect_to users_path, :alert => "Unable to update user."
+      redirect_to admin_users_path, :alert => "Unable to update user."
     end
   end
 
   def destroy
     unless user == current_user
       user.destroy
-      redirect_to users_path, :notice => "User deleted."
+      redirect_to admin_users_path, :notice => "User deleted."
     else
-      redirect_to users_path, :notice => "Can't delete yourself."
+      redirect_to admin_users_path, :notice => "Can't delete yourself."
     end
   end
 
   private
 
   def permitted_params
-    params.require(:user).permit(:email, :name)
+    params.require(:user).permit(:name)
   end
 end
--- spec/controllers/admin/users_controller_spec.rb
+++ spec/controllers/admin/users_controller_spec.rb
@@ -1,18 +1,79 @@
 require 'spec_helper'
 
+shared_context "some user exists" do
+  let!(:some_user) { create(:user) }
+end
+
+shared_context "params contain user id" do
+  include_context "some user exists"
+  before { controller.params[:id] = some_user.id }
+end
+
 describe Admin::UsersController do
   include_context 'logged in as user'
 
-  describe "GET 'show'" do
+  context 'exposures' do
+    describe '#decorated_collection' do
+      context "additional user exists" do
+        let!(:some_user) { create(:user) }
 
-    it "should be successful" do
+        it { expect(subject.decorated_collection).to include user }
+        it { expect(subject.decorated_collection).to include some_user }
+        it { expect(subject.decorated_collection).to have(2).items }
+      end
+    end
+
+    describe '#user' do
+      include_context "params contain user id"
+
+      it { expect(subject.user).to eq some_user }
+    end
+
+    describe '#decorated_user' do
+      include_context "params contain user id"
+
+      it { expect(subject.decorated_user).to be_decorated_with UserDecorator }
+      it { expect(subject.decorated_user.object).to eq some_user }
+    end
+  end
+
+  describe "GET 'show'" do
+    it 'should be successful' do
       get :show, id: user.id
       expect(response).to be_success
     end
 
-    it "should find the right user" do
+    it 'should find the right user' do
       get :show, id: user.id
       expect(subject.user).to eq user
     end
+  end
+
+  describe "PUT 'update'" do
+    context "given user exists" do
+      context "updating attributes" do
+        include_context "some user exists"
+        let(:new_name) { 'some_special_name' }
+        before { put :update, id: some_user.id, user: {name: new_name} }
+
+        it { expect(some_user.reload.name).to eq new_name }
+        it { expect(response).to redirect_to admin_users_path }
+      end
+    end
+  end
+
+  describe "DELETE 'destroy'" do
+    context do
+      include_context "some user exists"
+
+      it { expect { delete :destroy, id: some_user.id }.to change { User.count }.by(-1) }
+
+      context 'after request' do
+        before { delete :destroy, id: some_user.id }
+        it { expect(response).to redirect_to admin_users_path }
+      end
+    end
+
+    it { expect { delete :destroy, id: user.id }.to_not change { User.count }.by(-1) }
   end
 end
