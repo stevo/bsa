2c0fcab9897db45fedfdc1c031c2aacc9b652125
Stevo <b.kosmowski@selleo.com>
2013-09-09 22:59:58 +0200

    Tweaking registration + translations

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -12,7 +12,7 @@ class RegistrationsController < Devise::RegistrationsController
       else
         set_flash_message :notice, :"signed_up_but_#{resource.inactive_message}" if is_navigational_format?
         expire_session_data_after_sign_in!
-        respond_with resource, :location => after_inactive_sign_up_path_for(resource)
+        respond_with resource, location: new_user_session_path
       end
     else
       clean_up_passwords resource
