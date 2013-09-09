f07742a20acacebc29e82dca117a4444a0c8a0c7
Stevo <b.kosmowski@selleo.com>
2013-09-09 17:23:03 +0200

    Access denied exceptions should redirect straight to login screen

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,11 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-  rescue_from CanCan::AccessDenied, IntegrityException do |exception|
+  rescue_from CanCan::AccessDenied do |exception|
+    redirect_to new_user_session_path, alert: exception.message
+  end
+
+  rescue_from IntegrityException do |exception|
     redirect_to root_path, alert: exception.message
   end
 
