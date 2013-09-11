05b72310bd7bd7bf6a9c9adc62f1e655c6ad2d85
Stevo <b.kosmowski@selleo.com>
2013-09-11 16:04:00 +0200

    Remove action we do not want

--- app/controllers/admin/user/contributions_controller.rb
+++ app/controllers/admin/user/contributions_controller.rb
@@ -1,25 +1,21 @@
 class Admin::User::ContributionsController < Admin::AdminController
   inherit_resources
   belongs_to :user
-  actions :index, :new, :create, :destroy
+  actions :new, :create, :destroy
 
   def create
     super do |format|
-      format.html { redirect_to_index }
+      format.html { redirect_to :back }
     end
   end
 
   def destroy
     super do |format|
-      format.html { redirect_to_index }
+      format.html { redirect_to :back }
     end
   end
 
   private
-
-  def redirect_to_index
-    redirect_to admin_user_contributions_path(parent)
-  end
 
   def permitted_params
     params.permit(contribution: [:amount])
