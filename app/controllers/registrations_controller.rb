a16dfff3bdc8a8ba439bbdc7771a596f80ef083c
Stevo <b.kosmowski@selleo.com>
2013-09-17 16:07:19 +0200

    Allow saving avatar

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -22,6 +22,6 @@ class RegistrationsController < Devise::RegistrationsController
 
   def update_sanitized_params
     devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:name, :email, :password, :password_confirmation) }
-    devise_parameter_sanitizer.for(:account_update) { |u| u.permit(:name, :email, :password, :password_confirmation, :current_password) }
+    devise_parameter_sanitizer.for(:account_update) { |u| u.permit(:name, :email, :avatar, :password, :password_confirmation, :current_password) }
   end
 end
