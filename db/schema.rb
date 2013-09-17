324ef6da9f96c0fb18ead3ce0fc4b9b2803e555a
Stevo <b.kosmowski@selleo.com>
2013-09-17 16:34:34 +0200

    Update schema

--- db/schema.rb
+++ db/schema.rb
@@ -45,17 +45,6 @@ ActiveRecord::Schema.define(version: 20130917131732) do
   add_index "roles", ["name", "resource_type", "resource_id"], name: "index_roles_on_name_and_resource_type_and_resource_id", using: :btree
   add_index "roles", ["name"], name: "index_roles_on_name", using: :btree
 
-  create_table "settings", force: true do |t|
-    t.string   "var",                   null: false
-    t.text     "value"
-    t.integer  "thing_id"
-    t.string   "thing_type", limit: 30
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
-  add_index "settings", ["thing_type", "thing_id", "var"], name: "index_settings_on_thing_type_and_thing_id_and_var", unique: true, using: :btree
-
   create_table "users", force: true do |t|
     t.string   "email",                  default: "", null: false
     t.string   "encrypted_password",     default: ""
@@ -106,7 +95,7 @@ ActiveRecord::Schema.define(version: 20130917131732) do
 
   create_table "votings", force: true do |t|
     t.integer  "membership_id"
-    t.boolean  "closed",        default: false
+    t.boolean  "closed",        default: false, null: false
     t.date     "finishes_at"
     t.datetime "created_at"
     t.datetime "updated_at"
