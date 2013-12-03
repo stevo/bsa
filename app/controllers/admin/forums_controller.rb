fac961ce7b289477b81dac32946c088c0b11c13f
Paweł Damasiewicz <pawel.damasiewicz@gmail.com>
2013-12-03 17:04:15 +0100

    Removed ugly hack, and corrected typo #9

--- app/controllers/admin/forums_controller.rb
+++ app/controllers/admin/forums_controller.rb
@@ -1,10 +1,9 @@
 class Admin::ForumsController < ApplicationController
   layout 'master'
   expose(:forums, attributes: :permitted_params)
-  expose(:forum, atrributes: :permitted_params)
+  expose(:forum, attributes: :permitted_params)
 
   def update
-    params[:forum].each { |f| forum[f[0]] = f[1] }
     if forum.save
       redirect_to admin_forums_path
     else
@@ -12,8 +11,8 @@ class Admin::ForumsController < ApplicationController
     end
   end
 
-  public
+  private
   def permitted_params
-    params.permit(:name, :url, :login_url, :meeting_url, :state, :user, :password)
+    params.require(:forum).permit(:name, :url, :login_url, :meeting_url, :state, :user, :password)
   end
 end
